df6630d4641b8d0af438bb74322e80ba3f3b3471
Ye Chan Lin <yechanlin15703@gmail.com>
2026-07-26 20:30:15 -0700

    updated redeploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -6,9 +6,6 @@ cd ~/portfolio-project || exit 1
 # 2. Pull the latest changes from GitHub main
 git fetch && git reset origin/main --hard
 
-# 3. Enter venv and install dependencies
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
+docker compose -f docker-compose.prod.yml down
 
-# 4. Restart the systemd service to load the new code
-sudo systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml up -d --build
