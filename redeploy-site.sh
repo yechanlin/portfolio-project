2f5228077af9d2919d3d7aa7af7894d37dd4fff0
Ye Chan Lin <yechanlin15703@gmail.com>
2026-07-26 20:31:25 -0700

    updated redeploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -6,6 +6,6 @@ cd ~/portfolio-project || exit 1
 # 2. Pull the latest changes from GitHub main
 git fetch && git reset origin/main --hard
 
-docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yaml down
 
-docker compose -f docker-compose.prod.yml up -d --build
+docker compose -f docker-compose.prod.yaml up -d --build
